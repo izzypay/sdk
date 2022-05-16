2b4798eed8225df5665e55970f37387498822fa2
kukukk <szabolcs.balogh@lynxsolutions.eu>
2022-05-16 14:18:49 +0300

    Issue #1: create demo project

--- demo/demo.php
+++ demo/demo.php
@@ -2,16 +2,7 @@
 
 declare(strict_types=1);
 
-require('Traits/HmacTrait.php');
-require('Services/RequestService.php');
-require('Models/AbstractCustomer.php');
-require('Models/BasicCustomer.php');
-require('Models/DetailedCustomer.php');
-require('Models/Address.php');
-require('Models/CartItem.php');
-require('Models/Cart.php');
-require('Models/Other.php');
-require('IzzyPay.php');
+require_once('vendor/autoload.php');
 
 use IzzyPay\Exceptions\InvalidCustomerException;
 use IzzyPay\Exceptions\InvalidCartException;
@@ -28,24 +19,30 @@ use GuzzleHttp\Exception\GuzzleException;
 
 try {
     $merchantCartId = '666';
-    $izzyPay = new IzzyPay('merchantId', 'merchantSecret', 'https://www.izzypay.hu/bnpl');
+    $izzyPay = new IzzyPay('merchantId', 'merchantSecret', 'http://localhost:3333');
     $izzyPay->cred();
 
     $cartItem = CartItem::create('name','category', 'subCategory', 'type', 666.666, 69, 'manufacturer', 'merchantItemId', 'other');
     $cart = Cart::create('HUF', 666.666, [$cartItem]);
     $cart->reset();
     $cart->addItem($cartItem);
-    $basicCustomer = BasicCustomer::create('registered', 'merchantCustomerId', 'other');
-    $other = Other::create('ip', 'browser', 'os');
+    $basicCustomer = BasicCustomer::create('guest', 'merchantCustomerId', 'other');
+    $other = Other::create('127.0.0.1', 'browser', 'os');
     $izzyPay->init($merchantCartId, $cart, $basicCustomer, $other);
 
-    $address = Address::create('zip', 'city', 'street', 'houseNo', 'address1', 'address2', 'address3');
-    $detailedCustomer = DetailedCustomer::create('registered', 'merchantCustomerId', 'other', 'name', 'surname', 'phone', 'email', $address, $address);
+    $address = Address::create('8888', 'city', 'street', 'houseNo', 'address1', 'address2', 'address3');
+    $detailedCustomer = DetailedCustomer::create('guest', 'merchantCustomerId', 'other', 'name', 'surname', 'phone', 'email', $address, $address);
     $izzyPay->start($merchantCartId, $cart, $detailedCustomer, $other);
 } catch (InvalidCustomerException $e) {
+    var_dump($e->getMessage());
 } catch (InvalidCartException $e) {
+    var_dump($e->getMessage());
 } catch (InvalidOtherException $e) {
+    var_dump($e->getMessage());
 } catch (InvalidResponseException $e) {
+    var_dump($e->getMessage());
 } catch (GuzzleException $e) {
+    var_dump($e->getMessage());
 } catch (JsonException $e) {
+    var_dump($e->getMessage());
 }
