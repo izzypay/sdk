617557c71cbd2b94c1708ebbd2f8494d31f4dc94
kukukk <szabolcs.balogh@lynxsolutions.eu>
2022-05-17 11:17:47 +0300

    Issue #1: Refactor demo

--- demo/demo.php
+++ demo/demo.php
@@ -16,34 +16,66 @@ use IzzyPay\Models\Cart;
 use IzzyPay\Models\BasicCustomer;
 use IzzyPay\Models\DetailedCustomer;
 use IzzyPay\Models\Other;
-use GuzzleHttp\Exception\GuzzleException;
+use IzzyPay\Models\Urls;
 
-try {
-    $merchantCartId = '666';
-    $izzyPay = new IzzyPay('merchantId', 'merchantSecret', 'http://localhost:3333');
-    $izzyPay->cred();
+$token = null;
+$merchantCartId = '666';
+$izzyPay = new IzzyPay('merchantId', 'abcd1234', 'http://gateway.localhost');
 
-    $cartItem = CartItem::create('name','category', 'subCategory', 'type', 666.666, 69, 'manufacturer', 'merchantItemId', 'other');
-    $cart = Cart::create('HUF', 666.666, [$cartItem]);
-    $cart->reset();
-    $cart->addItem($cartItem);
-    $basicCustomer = BasicCustomer::create('guest', 'merchantCustomerId', 'other');
-    $other = Other::create('127.0.0.1', 'browser', 'os');
-    $izzyPay->init($merchantCartId, $cart, $basicCustomer, $other);
+function verifyCredential(IzzyPay $izzyPay): void
+{
+    try {
+        $izzyPay->cred();
+    } catch (InvalidResponseException|RequestException $e) {
+        var_dump($e->getMessage());
+    }
+}
 
-    $address = Address::create('8888', 'city', 'street', 'houseNo', 'address1', 'address2', 'address3');
-    $detailedCustomer = DetailedCustomer::create('guest', 'merchantCustomerId', 'other', 'name', 'surname', 'phone', 'email', $address, $address);
-    $izzyPay->start($merchantCartId, $cart, $detailedCustomer, $other);
-} catch (InvalidCustomerException $e) {
-    var_dump($e->getMessage());
-} catch (InvalidCartException $e) {
-    var_dump($e->getMessage());
-} catch (InvalidOtherException $e) {
-    var_dump($e->getMessage());
-} catch (InvalidResponseException $e) {
-    var_dump($e->getMessage());
-} catch (RequestException $e) {
-    var_dump($e->getMessage());
-} catch (JsonException $e) {
-    var_dump($e->getMessage());
+function sendInit(IzzyPay $izzyPay, string $merchantCartId): ?array
+{
+    try {
+        $cartItem = CartItem::create('name','category', 'subCategory', 'type', 666.666, 69, 'manufacturer', 'merchantItemId', 'other');
+        $cart = Cart::create('HUF', 666.666, [$cartItem]);
+        $basicCustomer = BasicCustomer::create('guest', 'merchantCustomerId', 'other');
+        $other = Other::create('127.0.0.1', 'browser', 'os');
+        return $izzyPay->init($merchantCartId, $cart, $basicCustomer, $other);
+    } catch (InvalidCustomerException|InvalidCartException|InvalidOtherException|InvalidResponseException|RequestException|JsonException $e) {
+        var_dump($e->getMessage());
+    }
+    return null;
+}
+
+function sendStart(IzzyPay $izzyPay, string $merchantCartId, $token): ?array
+{
+    try {
+        $cartItem = CartItem::create('name','category', 'subCategory', 'type', 666.666, 69, 'manufacturer', 'merchantItemId', 'other');
+        $cart = Cart::create('HUF', 666.666, [$cartItem]);
+        $address = Address::create('8888', 'city', 'street', 'houseNo', 'address1', 'address2', 'address3');
+        $detailedCustomer = DetailedCustomer::create('guest', 'merchantCustomerId', 'other', 'name', 'surname', 'phone', 'email@emai.com', $address, $address);
+        $other = Other::create('127.0.0.1', 'browser', 'os');
+        $urls = Urls::create('https://ipn.com');
+        return $izzyPay->start($token, $merchantCartId, $cart, $detailedCustomer, $other, $urls);
+    } catch (InvalidCustomerException|InvalidCartException|InvalidOtherException|InvalidResponseException|RequestException|JsonException $e) {
+        var_dump($e->getMessage());
+    }
+    return null;
+}
+
+verifyCredential($izzyPay);
+$initResponse = sendInit($izzyPay, $merchantCartId);
+if ($initResponse && !array_key_exists('errors', $initResponse) && $initResponse['available']) {
+    $jsUrl = $initResponse['jsUrl'];
+    $token = $initResponse['token'];
+    var_dump($jsUrl);
+} else {
+    var_dump($initResponse['errors']);
+}
+
+if ($token) {
+    $startResponse = sendStart($izzyPay, $merchantCartId, $token);
+    if ($startResponse && !array_key_exists('errors', $startResponse)) {
+        var_dump('Ok');
+    } else {
+        var_dump($startResponse['errors']);
+    }
 }
